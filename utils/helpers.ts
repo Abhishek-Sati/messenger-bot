4735aac1d4f80f1cea0b21e0c5fea8fa607d3177
Abhishek Sati <abhishek@5cnetwork.com>
2021-11-27 01:18:56 +0530

    heroku configuration

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,6 +1,4 @@
-import { NodeEnvironments, RedisCachedKeys } from "./types"
-import { redisClient } from "../config/redis"
-import { Response } from "express"
+import { NodeEnvironments } from "./types"
 import moment from "moment"
 
 
@@ -12,22 +10,22 @@ export const isDevelopment = NODE_ENV === NodeEnvironments.DEVELOPMENT
 // This is the helper function to call directly check if any value exist inside redis:
 // 1. if yes, return that value.
 // 2. else store value in redis and return the data.
-export const getRedisClientCache = (redisKey: RedisCachedKeys, cb: (args?: any) => Promise<any>, res?: Response, args?: any) => {
-    redisClient.get(redisKey, async (error, reply) => {
-        if (reply) {
-            const cachedData = JSON.parse(reply as string)
-            if (res) {
-                res.status(200).send({ data: cachedData, count: cachedData.length })
-            }
-        } else {
-            const fetchedData = await cb(args)
-            redisClient.set(redisKey, JSON.stringify(fetchedData))
-            if (res) {
-                res.status(200).send({ data: fetchedData, count: fetchedData.length })
-            }
-        }
-    })
-}
+// export const getRedisClientCache = (redisKey: RedisCachedKeys, cb: (args?: any) => Promise<any>, res?: Response, args?: any) => {
+//     redisClient.get(redisKey, async (error, reply) => {
+//         if (reply) {
+//             const cachedData = JSON.parse(reply as string)
+//             if (res) {
+//                 res.status(200).send({ data: cachedData, count: cachedData.length })
+//             }
+//         } else {
+//             const fetchedData = await cb(args)
+//             redisClient.set(redisKey, JSON.stringify(fetchedData))
+//             if (res) {
+//                 res.status(200).send({ data: fetchedData, count: fetchedData.length })
+//             }
+//         }
+//     })
+// }
 
 
 export const daysRemaining = (eventDate: string) => {
