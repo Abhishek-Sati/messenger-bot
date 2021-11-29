a7049612b31882c402330bf0bbe5799a3137bf24
Abhishek Sati <abhishek@5cnetwork.com>
2021-11-29 11:56:32 +0530

    NLP config updated

--- services/webhook.ts
+++ services/webhook.ts
@@ -2,37 +2,37 @@
 import request from 'request'
 
 
-export function handleMessage(sender_psid: string, received_message: { text: string }) {
-    let response
-
-    // Check if the message contains text
-    if (received_message.text) {
-
-        // Create the payload for a basic text message
-        response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
-        }
-    }
-
-    // Sends the response message
-    callSendAPI(sender_psid, response)
-}
+// function handleMessage(sender_psid: string, received_message: { text: string }) {
+//     let response
+//
+//     // Check if the message contains text
+//     if (received_message.text) {
+//
+//         // Create the payload for a basic text message
+//         response = {
+//             "text": `You sent the message: "${received_message.text}". Now send me an image!`
+//         }
+//     }
+//
+//     // Sends the response message
+//     callSendAPI(sender_psid, response)
+// }
 
 
 // Handles messaging_postbacks events
-export function handlePostback(sender_psid: string, received_postback: string) {
+function handlePostback(sender_psid: string, received_postback: string) {
 
 }
 
 
 // Sends response messages via the Send API
-export function callSendAPI(sender_psid: string, response: any) {
+function callSendAPI(sender_psid: string, response: any) {
     // Construct the message body
     let request_body = {
         "recipient": {
             "id": sender_psid
         },
-        "message": response
+        "message": { text: response }
     }
 
     // Send the HTTP request to the Messenger Platform
@@ -48,4 +48,27 @@ export function callSendAPI(sender_psid: string, response: any) {
             console.error("Unable to send message:" + err)
         }
     })
+}
+
+
+type NLPType = {
+    entities: any
+    traits: { [x: string]: any[] }
+}
+
+
+function firstTrait(nlp: NLPType, name: string) {
+    return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0]
+}
+
+
+function handleMessage(sender_psid: string, received_message: { text: string, nlp: NLPType }) {
+    // check greeting is here and is confident
+    const greeting = firstTrait(received_message.nlp, 'wit$greetings')
+    if (greeting && greeting.confidence > 0.8) {
+        callSendAPI(sender_psid, 'Hi there!')
+    } else {
+        // default logic
+        callSendAPI(sender_psid, 'Default Message')
+    }
 }
